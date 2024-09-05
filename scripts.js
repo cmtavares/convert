fea8109c436bd9a5d1aa9fd282cd5bda4286deda
Carlos Tavares <cmmt08@gmail.com>
2024-09-05 11:33:45 -0300

    feat: add currency conversion logic and improve error handling

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-const USD = 4.87
-const EUR = 5.32
-const GBP = 6.08
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
 
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulating the input amount to receive only numbers
 amount.addEventListener("input", () => {
@@ -17,17 +20,43 @@ form.onsubmit = (event) => {
 
   switch (currency.value) {
     case "USD":
-      convertCurrency(amount.value, USD, "US$")
-      break
+      convertCurrency(amount.value, USD, "US$");
+      break;
     case "EUR":
       convertCurrency(amount.value, EUR, "€");
-      break
+      break;
     case "GBP":
       convertCurrency(amount.value, GBP, "£");
-      break
+      break;
   }
 };
 
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol)
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    let total = amount * price;
+
+    if(isNaN(total)) {
+      return alert("Por favor, insira um valor válido.");
+    }
+
+    total = formatCurrencyBRL(total).replace("R$", "");
+
+    result.textContent = `${total} Reais`
+
+    footer.classList.add("show-result");
+  } catch (error) {
+    footer.classList.remove("show-result");
+
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde.");
+  }
+}
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }

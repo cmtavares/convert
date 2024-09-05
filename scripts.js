9b9177269ad0a6485668e2714c2831759ed59bca
Carlos Tavares <cmmt08@gmail.com>
2024-09-05 11:03:50 -0300

    feat: add currency conversion functionality

--- scripts.js
+++ scripts.js
@@ -1,7 +1,33 @@
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 // Manipulating the input amount to receive only numbers
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break
+  }
+};
+
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
